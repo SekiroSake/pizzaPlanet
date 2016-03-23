2e71c184ade7a4624532ac90c631cf5713c0a492
hang-wang <hwang12wou@gmail.com>
2016-03-23 16:13:12 -0400

    first commit

---
+++ js/controllers/MainController.js
@@ -0,0 +1,37 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.today = new Date();
+
+  $scope.appetizers = [
+    {
+      name: 'Caprese',
+      description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
+      price: 4.95
+    },
+    {
+      name: 'Mozzarella Sticks',
+      description: 'Served with marinara sauce.',
+      price: 3.95
+    },
+  {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil.',
+      price: 4.95
+    },
+  ];
+  $scope.mains=[
+    {
+      name:"apple",
+       description: 'Eella, tomatoes, basil, balsmaic glaze.',
+      price: 4.95
+  },{
+        name:"pear",
+     description: 'Basil, balsmaic glaze.',
+      price: 3.95
+  },{
+        name:"watermellon",
+     description: 'Tomatoes, basil, balsmaic glaze.',
+      price: 4.95
+  }
+               ];
+
+}]);
